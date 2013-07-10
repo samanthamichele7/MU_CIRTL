7c92431e9098b38cb2c2be4e94a3a6b6a64cdb4d
Samantha Geitz <samanthamichele7@gmail.com>
2013-07-09 22:02:54 -0500

    Initial commit

---
+++ front-page.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Template Name: Home Page Template
+*/
+?>
+
+<?php get_header(); ?>
+
+<div id="content">
+
+	<div id="inner-content" class="wrap clearfix">
+
+			<div id="main" class="twelvecol first clearfix" role="main">
+
+					<div class="eightcol first">
+						<img src="<?php echo the_field('banner_image'); ?>" alt="" style="width: auto">
+					</div>
+
+					<div class="fourcol upcoming-events box-shadow">
+						<?php echo the_field('upcoming_events'); ?>
+					</div>
+
+				<div class="twelvecol first">
+					<?php echo the_field('preparing_stem_faculty'); ?>
+				</div>
+
+			</div> <!-- end #main -->
+
+
+	</div> <!-- end #inner-content -->
+
+</div> <!-- end #content -->
+
+<?php get_footer(); ?>
